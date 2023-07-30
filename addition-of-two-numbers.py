fa98c550a7cb0927953d99a0956da6ab3b437483
anvishaanand <138240758+anvishaanand@users.noreply.github.com>
2023-07-30 18:49:34 +0530

    Add files via upload

---
+++ addition-of-two-numbers.py
@@ -0,0 +1,7 @@
+def addition(a,b):
+    c=a+b
+    return(c)
+a=(float(input("enter the first number:-")) )
+b=(float(input("enter the second number:-")) )
+
+print("the addition of",a, "and", b, "is", addition(a,b))
