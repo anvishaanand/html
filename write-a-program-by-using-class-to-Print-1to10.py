dad8daae9d7f21a6de2d78c0829982024fb0ca8c
anvishaanand <138240758+anvishaanand@users.noreply.github.com>
2023-07-30 18:52:21 +0530

    Add files via upload

---
+++ write-a-program-by-using-class-to-Print-1to10.py
@@ -0,0 +1,11 @@
+class Number_Print:
+    def NUMBERS(self):
+        for x in range (1,11):
+            print(x)
+#create an instance of class
+printer = Number_Print()
+#Call the method to print the numbers 
+printer.NUMBERS()
+
+
+        
