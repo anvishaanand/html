4fbbc83dcd77705e283f9242b50009006a1d634d
anvishaanand <138240758+anvishaanand@users.noreply.github.com>
2023-07-31 09:21:23 +0530

    Add files via upload

---
+++ subtraction-by-using-function.py
@@ -0,0 +1,8 @@
+def subtraction(a,b):
+    c=a-b
+    return(c)
+
+a=(float(input("enter the first number:-")) )
+b=(float(input("enter the second number:-")) )
+
+print("the subtraction of", a , "and", b , "is", subtraction(a,b))
