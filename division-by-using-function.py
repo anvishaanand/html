7451f4c93b59ec6118136d87964603a25b99b04f
anvishaanand <138240758+anvishaanand@users.noreply.github.com>
2023-08-04 14:04:06 +0530

    Add files via upload

---
+++ division-by-using-function.py
@@ -0,0 +1,8 @@
+def division(a,b):
+    c=a-b
+    return(c)
+
+a=(float(input("enter the first number:-")) )
+b=(float(input("enter the second number:-")) )
+
+print("the division of", a , "and", b , "is", division(a,b))
