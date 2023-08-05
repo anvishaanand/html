0d9aa86d376a2cf4731d58966b394bd574f1263a
anvishaanand <138240758+anvishaanand@users.noreply.github.com>
2023-08-05 18:39:49 +0530

    Add files via upload

--- string1.py
+++ string1.py
@@ -1,3 +1,7 @@
 var1="Anandi vishal"
+#index value of "Anandi vishal"=0,1,2,3,4,5,6,7,8,9,10,11,12
+#we need letter "An" so we write take fron var1 from index value 0 to 2(2 in not included)
+# then we use "end" to stop going in the next line  
 print(var1[0:2],end="")
+#now we are going to take "visha" from var1 whose index value is 7 to 12 where(12 is not included)
 print(var1[7:12])
